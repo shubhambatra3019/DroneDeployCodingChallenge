b80b07a9b5d047ede36039cfa2932a23b1375891
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2017-11-01 15:08:51 -0400

    Learnt and used various dronedeploy apis to get data

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-new DroneDeploy({version: 1}).then(function(api){
-  console.log('DroneDeploy Api: ', api);
+new DroneDeploy({version: 1}).then(function(dronedeploy){
+  console.log('DroneDeploy Api: ', dronedeploy);
 });
+
+// initialises the drone deploy api 
+// adds an Event Listener to the button element
+// the button invokes the pdfListener function
+function droneApiReady(){
+  return new Promise((resolve) => {
+    window.dronedeploy.onload(() => {
+       $(.button).addEventListener("click", func);
+    });
+  });
+}
+
+// test to see if the button works
+function func() {
+	$(#text).innerHTML = "Test Button";
+}
+
+// returns the currenty view plan using the function getCurrentlyViewed in Plan API.
+function getCurrentPlan() {
+	return window.dronedeploy.Plans.getCurrentlyViewed();
+}
+
+//takes a parameter of plan object which will be currently viewed plan in our case
+//returns the tile data using the planId of the currently viewed plan, layerName as ortho and zoom level as 16.
+function fetchTileDataFromPlan(plan) {
+	return window.dronedeploy.Tiles.get({planId: plan.id, layerName: "ortho", zoom: 16})
+}
+
+//takes a parameter of plan object.
+//returns annotations that plan has on it using the planId.
+function getAnnotationsFromPlan(plan) {
+	return window.dronedeploy.Annotations.get(plan.id);
+}
+
