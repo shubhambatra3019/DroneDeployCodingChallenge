de6e13c85e124abc280f8f4ada5fd7cf75906c56
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2017-11-02 12:05:21 -0400

    Generating PDF of the map in view

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ new DroneDeploy({version: 1}).then(function(dronedeploy){
 function droneApiReady(){
   return new Promise((resolve) => {
     window.dronedeploy.onload(() => {
-       $(.button).addEventListener("click", func);
+       $(.button).addEventListener("click", generatePDFMain);
     });
   });
 }
 
 // test to see if the button works
-function func() {
+/*function func() {
 	$(#text).innerHTML = "Test Button";
 }
+*/
 
 // returns the currenty view plan using the function getCurrentlyViewed in Plan API.
 function getCurrentPlan() {
@@ -26,7 +27,7 @@ function getCurrentPlan() {
 //takes a parameter of plan object which will be currently viewed plan in our case
 //returns the tile data using the planId of the currently viewed plan, layerName as ortho and zoom level as 16.
 function fetchTileDataFromPlan(plan) {
-	return window.dronedeploy.Tiles.get({planId: plan.id, layerName: "ortho", zoom: 16})
+	return window.dronedeploy.Tiles.get({planId: plan.id, layerName: "ortho", zoom: 16});
 }
 
 //takes a parameter of plan object.
@@ -39,19 +40,19 @@ function getAnnotationsFromPlan(plan) {
 function sendDataToServer(geometry, tileResponse, annotations) {
   //data that is being sent to the server
   var data = {
-    tiles: tileResponse.tiles;
-    planGeo: geometry;
-    zoom_level: 16;
-    annotations: annotations;
+    tiles: tileResponse.tiles,
+    planGeo: geometry,
+    zoom_level: 16,
+    annotations: annotations
   };
 
   //sends the data to server using the HTTP POST method.
   //return a response object
-  return fetch("https://dronedeploy-pdf-generator.herokuappcom/", {
-
+  return fetch("https://dronedeploy-pdf-generator.herokuapp.com/", {
+      
       method: "POST";
-
       body: JSON.stringify(data)
+  
   });
 
 }
@@ -75,4 +76,33 @@ function readBlob(responseBlob) {
   });
 } 
 
+//makes an object with the data and positions the map on the PDF canvas and saves the file.
+function generatePDFOfMap(plan, reader) {
 
+  response = JSON.parse(reader.result);
+  width = response.new_width/10;
+  left_margin = (180-width)/2 + 15;
+  height = response.new_height/10;
+
+  var document = new jsPDF("p");
+  document.text(plan.name, left_margin, 30);
+  document.addImage(response.image, "JPEG", left_margin, 40, width, height);
+  document.save(plan.name + ".pdf");
+
+}
+
+//calls all the functions that get the data, send it to server and use the response to convert it into a pdf.
+function generatePDFMain() {
+
+  getCurrentPlan()
+    .then(plan => fetchTileDataFromPlan(plan)
+    .then(tileResponse => getAnnotationsFromPlan(plan)
+    .then(annotations => sendDataToServer(plan.geometry, tileResponse, annotations)
+    .then(response => getResponseBlob(response)
+    .then(responseBlob => readBlob(responseBlob)
+    .then(fileReader => generatePDFOfMap(plan, fileReader)
+  ))))))
+
+}
+
+droneApiReady();
